60c522743b3cc08861b9589274b890f0f04f4347
Sahil <sahilsingh7977@gmail.com>
2022-08-20 13:16:18 +0530

    modified urls

--- blog/urls.py
+++ blog/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from blog.blog.settings import STATIC_ROOT
+from blog.settings import STATIC_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
